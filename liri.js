076fbd85ead0ec5193e1db8e1f57aef779c42713
Thomas Bentley <bentleygtr69@yahoo.com>
2018-07-21 20:23:49 -0500

    finished for the most part

--- liri.js
+++ liri.js
@@ -3,11 +3,13 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var fs = require("fs");
 var Twitter = require("twitter");
-var
+var request = require("request");
 
- Spotify = require("node-spotify-api");
+Spotify = require("node-spotify-api");
 var nodeArg = process.argv;
 var action = nodeArg[2];
+var searchTerms = process.argv.slice(3).join(" ");
+var divider = "\n\n───────────────────────────────────────────";
 
 
 var spotify = new Spotify(keys.spotify);
@@ -16,7 +18,8 @@ var client = new Twitter(keys.twitter);
 
 
 function spotifyAPI() {
-    var args = {type: 'track', query: nodeArg[3]}
+    
+    var args = {type: 'track', query: searchTerms}
 
     spotify.search(args, function(error, data){
 
@@ -53,12 +56,59 @@ function twitterAPI() {
         });
 };
 
+function movieAPI () {
+    var queryUrl = "http://www.omdbapi.com/?t=" + searchTerms + "&y=&plot=short&apikey=trilogy"
 
+    request(queryUrl, function(error, response, body){
+        if (!error && response.statusCode === 200){
+            var jsonData = JSON.parse(body);
+            //console.log(jsonData);
+            var title = jsonData.Title;
+            var year = jsonData.Year;
+            var ratingIMDB = jsonData.imdbRating;
+            var ratingRotten = jsonData.tomatoMeter;
+            var country = jsonData.Country;
+            var lang = jsonData.Language;
+            var plot = jsonData.Plot;
+            var cast = jsonData.Actors;
 
+            var movieData = [
+                "\nTitle: " + title,
+                "Year: " + year,
+                "IMDB Rating: " + ratingIMDB,
+                "Tomato Meter: " + ratingRotten,
+                "Country: " + country,
+                "Language: " + lang,
+                "Plot: " + plot,
+                "Cast: " + cast,
+            ].join("\n\n");
+
+            // fs.appendFile("random.txt", divider + movieData, function(error){
+            //     if (error) throw error;
+            // })
+            console.log(divider + movieData)
+            //console.log(jsonData);
+        }
+    })
+}
+    function randomText () {
+        fs.readFile("./random.txt", "utf-8", function(error, data){
+            if (error){
+                return console.log(error);
+            }
+            nodeArg[3] = response.data;
+
+        })
+    }
 if (action === "spotify-this-song"){
     spotifyAPI();
 }
  if (action === "my-tweets") {
     twitterAPI();
 }
-
+if (action === "movie-this"){
+    movieAPI();
+}
+if (action === "do-what-it-says"){
+    randomText();
+}
